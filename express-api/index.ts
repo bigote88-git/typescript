c4bd832c11c448011b6502e7cb9ea08ed9dbffce
djcortez <djcortez@inss.gob.ni>
2023-10-20 16:27:19 -0600

    Finalizando curso de Typescript

---
+++ express-api/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    id: 202,
+    message: 'New server'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
